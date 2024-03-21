f0f4e76192dd06a00c50d3ee65833c4ceba3f252
Shah hussain <shahhussaint6@gmail.com>
2024-03-21 15:59:57 +0500

    project006 completed

---
+++ project006/index.js
@@ -0,0 +1,4 @@
+console.log("Addition: 6 + 2 = ".concat(6 + 2));
+console.log("Subtrication: 12 - 4 = ".concat(12 - 4));
+console.log("Muliplication: 16 / 2 = ".concat(16 / 2));
+console.log("Addition: 4 * 2 = ".concat(4 * 2));
---
+++ project006/index.ts
@@ -0,0 +1,4 @@
+console.log(`Addition: 6 + 2 = ${6+2}`)
+console.log(`Subtrication: 12 - 4 = ${12-4}`)
+console.log(`Muliplication: 16 / 2 = ${16/2}`)
+console.log(`Addition: 4 * 2 = ${4*2}`)
