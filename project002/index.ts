c2cbf07ed299c52e15e13ea154d0a7cd2e1442d5
Shah hussain <shahhussaint6@gmail.com>
2024-03-21 16:06:30 +0500

    project002 completed

--- project002/index.ts
+++ project002/index.ts
@@ -1,3 +1,6 @@
+
+// Q3)Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
 let personName:string="Shah hussain"
 
 let lowerCaseName:string=personName.toLowerCase()
