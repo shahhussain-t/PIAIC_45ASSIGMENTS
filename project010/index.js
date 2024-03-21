d772851348b99c464e4a1b46304c62551d89f984
Shah hussain <shahhussaint6@gmail.com>
2024-03-21 16:41:51 +0500

    project010 completed

--- project010/index.js
+++ project010/index.js
@@ -1,6 +1,8 @@
-// Q10) Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-var names = ["Shahwaiz Tahir", 'Manzar', 'Saifullah Ghori', 'Syed Maaz'];
-console.log("First friend Name: ".concat(names[0]));
-console.log("Second friend Name: ".concat(names[1]));
-console.log("Third friend Name: ".concat(names[2]));
-console.log("Fourth friend Name: ".concat(names[3]));
+// Q11) Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+{
+    var names = ["Shahwaiz Tahir", 'Manzar', 'Saifullah Ghori', 'Syed Maaz'];
+    console.log("First friend Name: ".concat(names[0]));
+    console.log("Second friend Name: ".concat(names[1]));
+    console.log("Third friend Name: ".concat(names[2]));
+    console.log("Fourth friend Name: ".concat(names[3]));
+}
--- project010/index.ts
+++ project010/index.ts
@@ -1,10 +1,13 @@
 
 // Q11) Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+{
 
-let names:string[]=["Shahwaiz Tahir",'Manzar','Saifullah Ghori','Syed Maaz']
+    let names:string[]=["Shahwaiz Tahir",'Manzar','Saifullah Ghori','Syed Maaz']
+    
+    
+    console.log(`First friend Name: ${names[0]}`)
+    console.log(`Second friend Name: ${names[1]}`)
+    console.log(`Third friend Name: ${names[2]}`)
+    console.log(`Fourth friend Name: ${names[3]}`)
 
-
-console.log(`First friend Name: ${names[0]}`)
-console.log(`Second friend Name: ${names[1]}`)
-console.log(`Third friend Name: ${names[2]}`)
-console.log(`Fourth friend Name: ${names[3]}`)
+}
