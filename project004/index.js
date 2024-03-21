6a5f18a22525d9a67d05ab8da2796554b746bb2e
Shah hussain <shahhussaint6@gmail.com>
2024-03-21 15:32:57 +0500

    project004 completed

---
+++ project004/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const famous_quote = `"The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart."`;
+const famous_person = "Helen Keller";
+let message = `${famous_person} once said, ${famous_quote}`;
+console.log(message);
---
+++ project004/index.ts
@@ -0,0 +1,7 @@
+
+const famous_quote:string=`"The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart."`
+const famous_person:string="Helen Keller"
+
+let message:string=`${famous_person} once said, ${famous_quote}`
+
+console.log(message)
