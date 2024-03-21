2bb2b26a22dd57e7e30216535e32489e112f76c8
Shah hussain <shahhussaint6@gmail.com>
2024-03-21 15:33:53 +0500

    project002 completed

--- project002/index.ts
+++ project002/index.ts
@@ -1,8 +1,8 @@
-let personName="Shah hussain"
+let personName:string="Shah hussain"
 
-let lowerCaseName=personName.toLowerCase()
-let upperCaseName=personName.toUpperCase()
-let titleCaseName=personName.split(' ').map(name=>name[0].toUpperCase() +name.slice(1).toLowerCase()).join(' ')
+let lowerCaseName:string=personName.toLowerCase()
+let upperCaseName:string=personName.toUpperCase()
+let titleCaseName:string=personName.split(' ').map(name=>name[0].toUpperCase() +name.slice(1).toLowerCase()).join(' ')
 
 console.log(`Name in lowercase ${lowerCaseName}.`)
 console.log(`Name in uppercase ${upperCaseName}.`)
