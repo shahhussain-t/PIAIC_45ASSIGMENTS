8a38fe97126ccbbbbab003379b9476f6e1fb3144
Shah hussain <shahhussaint6@gmail.com>
2024-03-21 16:09:00 +0500

    project006 completed

--- project006/index.ts
+++ project006/index.ts
@@ -1,3 +1,6 @@
+// Q7)Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+
 console.log(`Addition: 6 + 2 = ${6+2}`)
 console.log(`Subtrication: 12 - 4 = ${12-4}`)
 console.log(`Muliplication: 16 / 2 = ${16/2}`)
