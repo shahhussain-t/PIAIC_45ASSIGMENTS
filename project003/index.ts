876df588654157190011041b84d90b7365f38cac
Shah hussain <shahhussaint6@gmail.com>
2024-03-21 16:07:23 +0500

    project003 completed

--- project003/index.ts
+++ project003/index.ts
@@ -1,4 +1,9 @@
 
+// Q4) Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+
 const famousQuote:string=`"The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart."`
 const authorName:string="Helen Keller"
 
