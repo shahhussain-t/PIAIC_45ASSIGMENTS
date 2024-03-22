a36271abefefb94fb3226c2be80d39c36df7b809
Shah hussain <shahhussaint6@gmail.com>
2024-03-22 17:11:19 +0500

    project030 completed

--- README.md
+++ README.md
@@ -303,6 +303,19 @@ console.log(car == 'subaru')
 <hr>
 
 
+<h1>project030</h1>
+
+<p>31.No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+<br>
+• If the list is empty, print the message We need to find some users!
+<br>
+
+• Remove all of the usernames from your array, and make sure the correct message is printed.
+</p>
+
+<hr>
+
+
 
 
 
---
+++ project030/index.js
@@ -0,0 +1,34 @@
+"use strict";
+// Q31) No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+//• If the list is empty, print the message We need to find some users!
+Object.defineProperty(exports, "__esModule", { value: true });
+//• Remove all of the usernames from your array, and make sure the correct message is printed.
+let usernames = ["Admin", "Shah hussain", "Yasir", "Usman", "Basit"];
+if (usernames.length === 0) {
+    console.log('We need to find some users!');
+}
+else {
+    usernames.forEach((username) => {
+        if (username === "Admin") {
+            console.log(`Hello ${username}, would you like to see a status report?`);
+        }
+        else {
+            console.log(`Hello ${username}, thank you for logging in again.`);
+        }
+    });
+}
+usernames = [];
+console.log('*** empty array***');
+if (usernames.length === 0) {
+    console.log('We need to find some users!');
+}
+else {
+    usernames.forEach((username) => {
+        if (username === "Admin") {
+            console.log(`Hello ${username}, would you like to see a status report?`);
+        }
+        else {
+            console.log(`Hello ${username}, thank you for logging in again.`);
+        }
+    });
+}
