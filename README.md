4ede3f95c42a971d7af3087eb03ba15224f6809e
Shah hussain <shahhussaint6@gmail.com>
2024-03-22 15:00:05 +0500

    project022 completed

--- README.md
+++ README.md
@@ -171,6 +171,23 @@
 
 <hr>
 
+<h1>project023</h1>
+
+<p>23.Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+<br>
+let car = 'subaru';
+<br>
+console.log("Is car == 'subaru'? I predict True.")
+<br>
+console.log(car == 'subaru')
+<br>
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+<br>
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+<hr>
+
+
 
 
 
---
+++ project022/index.ts
@@ -0,0 +1,70 @@
+// Q23) Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+
+let car:string = 'subaru';
+let country:string="pakistan"
+let age:number=26
+let isAlive:boolean=true
+let isNull=null
+let novalue=undefined
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car ==='subaru')
+
+console.log("Is age == 26? I predict True.")
+
+console.log(age ===26)
+
+
+
+console.log("Is isAlive == true? I predict True.")
+
+console.log(isAlive===true)
+
+
+console.log("Is isNull == null? I predict True.")
+
+console.log(isNull==null)
+
+console.log("Is !isNan(age)? I predict True.")
+console.log(!isNaN(age),)
+
+console.log("Is isNull == null? I predict True.")
+
+console.log(isNull==null)
+
+console.log("Is novalue == undefine? I predict True.")
+
+console.log(novalue==undefined)
+
+
+
+
+
+console.log("Is country == 'subaru'? I predict False.")
+
+console.log(country === 'subaru')
+
+
+console.log("Is age == 26? I predict False.")
+
+console.log(age ===25)
+
+console.log("Is isAlive !== true? I predict false.")
+
+console.log(isAlive !==true)
+
+
+console.log("Is isNull !== null? I predict False.")
+
+console.log(isNull !==null)
+
+console.log("Is isNan(age)? I predict False.")
+console.log(isNaN(age))
+
+
+
+
+
+
