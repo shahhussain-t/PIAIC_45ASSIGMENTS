d77c364b5bb87bbda2f2c2ea2b964e8eded071b3
Shah hussain <shahhussaint6@gmail.com>
2024-03-23 20:47:42 +0500

    node project upload

---
+++ NODE_PROJECTS/README.md
@@ -0,0 +1,12 @@
+<div>
+    <h1>project00_calculator</h1>
+    <h2>Simple Command Line Calculator</h2>
+    <p>Develop a simple command line calculator using TypeScipt, Node.js and Inquirer.</p>
+    <h3>NPX command:</h3>
+    <p><b>npx shah-calculator</b></p>
+    <h3>output:</h3>
+    <div>
+        <img src="../assets/images/project00_calculator-output.PNG"  width="500">
+    </div>
+    
+</div>
---
+++ NODE_PROJECTS/project00_calculator/index.js
@@ -0,0 +1,66 @@
+#! /usr/bin/env  node
+import inquirer from "inquirer";
+import chalk from 'chalk';
+const log = console.log;
+const answer = await inquirer.prompt([
+    { name: "firstNumber", type: "number", message: "Enter first Number" },
+    { name: "secondNumber", type: "number", message: "Enter second Number" },
+    { choices: ["Addition", 'Subtraction', "Multplication", "Division"], type: "list", message: "Select The operator", name: "Operator" },
+]);
+const { firstNumber, secondNumber, Operator } = answer;
+switch (Operator) {
+    case "Addition":
+        log(chalk.blue(`
+
+            ${firstNumber}`)
+            +
+                chalk.red(` + `)
+            +
+                chalk.blue(`${secondNumber}`)
+            +
+                chalk.yellow(` = `)
+            +
+                chalk.green(`${firstNumber + secondNumber} `));
+        break;
+    case "Subtraction":
+        log(chalk.blue(`
+
+        ${firstNumber}`)
+            +
+                chalk.red(` - `)
+            +
+                chalk.blue(`${secondNumber}`)
+            +
+                chalk.yellow(` = `)
+            +
+                chalk.green(`${firstNumber - secondNumber} `));
+        break;
+    case "Multplication":
+        log(chalk.blue(`
+
+        ${firstNumber}`)
+            +
+                chalk.red(` * `)
+            +
+                chalk.blue(`${secondNumber}`)
+            +
+                chalk.yellow(` = `)
+            +
+                chalk.green(`${firstNumber * secondNumber} `));
+        break;
+    case "Division":
+        log(chalk.blue(`
+
+        ${firstNumber}`)
+            +
+                chalk.red(` / `)
+            +
+                chalk.blue(`${secondNumber}`)
+            +
+                chalk.yellow(` = `)
+            +
+                chalk.green(`${firstNumber / secondNumber} `));
+        break;
+    default:
+        console.log(`ENter a valid number or select a valid operator`);
+}
