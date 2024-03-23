bf8f419e1f62fc2fdc03c2bd28db69762be325ed
Shah hussain <shahhussaint6@gmail.com>
2024-03-23 21:37:31 +0500

    project001_number_guessing-game

---
+++ NODE_PROJECTS/project01_number_guessing-game/index.ts
@@ -0,0 +1,86 @@
+#! /usr/bin/env  node
+import inquirer from "inquirer";
+
+async function startGuessingGame() {
+
+    let attempt = 0
+    let play = true
+
+    while (play) {
+
+        let answers = await inquirer.prompt([
+
+
+            { message: "Enter your Guess number between 1 - 6:", type: "number", name: "guessNumber" }
+
+
+        ])
+
+
+        const randomNUmber = Math.floor(Math.random() * 6 + 1)
+
+
+
+        if (answers.guessNumber === randomNUmber) {
+
+            console.log(` you Guess the right number you won`)
+            let answers = await inquirer.prompt([
+
+
+                { choices: ["yes", 'no'], type: "list", message: "want to play again?", name: "Operator" },
+
+            ])
+
+            if (answers.Operator === "yes") {
+
+                play = true
+                attempt = 0
+
+
+            } else {
+
+                play = false
+            }
+
+        } else {
+
+            attempt++
+
+            if (attempt === 3) {
+
+                console.log('you losed the game better luck next time')
+                let answers = await inquirer.prompt([
+
+
+                    { choices: ["yes", 'no'], type: "list", message: "want to play again?", name: "Operator" },
+
+                ])
+
+                if (answers.Operator === "yes") {
+
+                    play = true
+                    attempt = 0
+
+
+                } else {
+
+                    play = false
+                }
+
+
+            } else {
+
+                console.log("you guess the wrong number", 'chances left', 3 - attempt)
+
+                play = true
+
+
+            }
+
+        }
+
+    }
+
+}
+
+startGuessingGame()
